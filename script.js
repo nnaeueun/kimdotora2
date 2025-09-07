6870bbc063bd97054917a1fdbc3709445aa3c397
김나은 <50188040+nnaeueun@users.noreply.github.com>
2025-09-07 20:53:23 +0900

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ document.querySelectorAll("section ul li").forEach(function(li) {
     }
   });
 });
+
+//비번 입력
+
+function checkPassword() {
+  const input = document.getElementById("password").value;
+  const content = document.getElementById("secret");
+  if (input === "1123") {  // master가 설정할 비밀번호
+    content.style.display = "block";
+  } else {
+    alert("비밀번호가 틀렸습니다.");
+  }
+}
